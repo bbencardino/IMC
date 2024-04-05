68749abff390016f37a544a8eb0c624c05c78f0c
Isabella Bencardino <isabella@bencardino.com.br>
2024-04-05 19:54:10 +0100

    Create base project

---
+++ IMC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IMC
+//
+//  Created by Isabella Bencardino on 05/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IMC/IMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMCApp.swift
+//  IMC
+//
+//  Created by Isabella Bencardino on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct IMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
